fd37fe1c48e315d64ce9ebc34a9ec5cd3ddd0ed2
Robin de Mourat <robin.demourat@gmail.com>
2016-09-14 12:12:12 +0200

    fix issues w/ async db population and clean code

--- dataToMongo.js
+++ dataToMongo.js
@@ -3,69 +3,122 @@ var fs = require('fs');
 var async = require('async');
 var MongoClient = require('mongodb').MongoClient
 
-var index = 0;
-
+var index = 0; // control index
 var url = 'mongodb://localhost:27017/myproject';
+var objectsToProcess = []; // list of json objects to process
 
-var authors, artworks;
-
-function insertArtwork(artwork, db, callback) {
-  // Insert artworks and call back
-
-  var artworkSimplified = artwork.ua;
-  artworkSimplified._id = artwork._id;
-
-  artworks.insert(artworkSimplified, {}, callback);
-}
-
-
+/**
+ * Format and insert an artwork's array of authors objects in authors mongo collection
+ */
 function insertAuthors(artwork, db, callback) {
-  // Insert all related authors
-  authors.insert(artwork._source.ua.authors, {}, callback);
+    var authors = artwork._source.ua.authors;
+    db.collection('authors').insert(authors,  function(err) {
+    	// not catching errors because possible dupkeys that we don't care about
+    	callback(null);
+    });
 }
 
-function onNode(artwork, db, callback) {
-	if (index % 100 === 0) {
-		console.log('on node', index);
-	}
-	// chaining the two insert operations
-	async.waterfall([
-		function(cback1) {
-			insertArtwork(artwork, db, cback1);
-		},
-		function(db, cback2) {
-			insertAuthors(artwork, db, cback2);
-		}
-	], callback);
-};
+/**
+ * Format and insert artwork object in artworks mongo collection
+ */
+function insertArtwork(artwork, db, callback) {
+    var artworkSimplified = artwork._source.ua;
+    artworkSimplified._id = artwork._id;
+    db.collection('artworks').insert(artworkSimplified, function(err){
+    	// not catching errors because possible dupkeys that we don't care about
+    	callback(null);
+    });
+}
 
-async.waterfall([
-	function(callback) {
-		console.log('connecting');
-		MongoClient.connect(url, callback);
-	},
-	function(db, callback) {
-		db.createCollection('authors', {}, callback);
-  		artworks = db.collection('documents');
-  		authors = db.collection('authors');
-	},
-	function (db ,callback) {
-		console.log('starting to feed data');
-		oboe(fs.createReadStream('./data/artworks-all.json'))
-			.node('![*]', function(node) {
-				index++;
-				onNode(node, db, function(err) {
-					// if (err)
-					// 	console.log('err on node', err);
-				});
-			})
-			.done(function(big){
-				console.log('done');
-				console.log('I am still alive ');
-				callback(db);
-			})
-	}
-], function(err, db) {
-	console.log('done, errors: ', err);
-	db.close();
-})
+/**
+ * Populate db with a native artwork data
+ */
+function populateDbWithArtwork(artwork, db, callback) {
+    index++;
+    if (index % 10000 === 0) {
+        console.log('populating db with artwork n°%s', index);
+    }
+    // chaining the two insert operations
+    async.waterfall([
+        function(cback1) {
+            insertArtwork(artwork, db, cback1);
+        },
+        function(cback2) {
+            insertAuthors(artwork, db, cback2);
+        }
+    ], function(err) {
+        if (index % 10000 === 0) {
+            console.log('done populating with artwork n°%s', index);
+        }
+        callback(err, artwork);
+    });
+}
+
+/**
+ * Main function
+ */
+function dataToMongo() {
+    async.waterfall([
+        // connect to mongo
+        function(callback) {
+            console.log('connecting to db');
+            MongoClient.connect(url, callback);
+        },
+        function(db, callback) {
+            // chaining the two collections creation operations
+            async.waterfall([
+                function(cback1) {
+                    console.log('creating authors collections');
+                    db.createCollection('authors', {}, cback1);
+                },
+                function(col, cback2) {
+                    console.log('creating artworks collections');
+                    db.createCollection('artworks', {}, cback2);
+                }
+            ], function(err, collection) {
+                console.log('collections (re) created, errors: ', err);
+                callback(err, db);
+            });
+        },
+        function(db, callback) {
+            console.log('clearing collections');
+            var authors = db.collection('authors');
+            // clear previous documents
+            authors.removeMany();
+            var artworks = db.collection('artworks');
+            // clear previous documents
+            artworks.removeMany();
+            callback(null, db);
+        },
+        function(db, callback) {
+            console.log('starting to parse data in json');
+            oboe(fs.createReadStream('./data/artworks-all.json'))
+                .node('![*]', function(node) {
+                    index++;
+                    objectsToProcess.push(node);
+                    if (index % 10000 === 0) {
+                        console.log('done parsing node n°%s', index);
+                    }
+                })
+                .done(function(results) {
+                    index = 0;
+                    console.log('done with streaming json');
+                    callback(null, db);
+                });
+        },
+        function(db, callback) {
+            console.log('starting populating the mongo base with %s artwork objects', objectsToProcess.length);
+            async.mapSeries(objectsToProcess, function(node, nodeCallback) {
+                populateDbWithArtwork(node, db, nodeCallback);
+            }, function(errors) {
+                console.log('done populating with all objects');
+                callback(errors, db);
+            })
+        }
+    ], function(err, db) {
+        console.log('done with everything, closing db');
+        db.close();
+    });
+}
+
+dataToMongo();
