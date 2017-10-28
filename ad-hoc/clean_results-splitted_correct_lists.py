e486c1ed15c6abde99efa7791f13e185a955b621
samaingw <samain.gwenael@laposte.net>
2017-10-28 14:11:19 +0200

    Ready-to-useness improvements

--- ad-hoc/clean_results-splitted_correct_lists.py
+++ ad-hoc/clean_results-splitted_correct_lists.py
@@ -8,13 +8,18 @@ from get_html_list import *
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         sys.exit("USAGE: ./assemble_results sourceDir destDir")
-    results = []
+    #results = []
     arbre = os.walk(sys.argv[1])
     sourceDir = sys.argv[1] if sys.argv[1][-1] == '/' else sys.argv[1] + '/'
     destDir = sys.argv[2] if sys.argv[2][-1] == '/' else sys.argv[2] + '/'
+    #sourceDir = os.path.abspath(sys.argv[1])
+    #destDir = os.path.abspath(sys.argv[2])
+    if not os.path.exists(sys.argv[2]):
+	    os.mkdir(sys.argv[2])
     #dir_dest = os.path.abspath(sys.argv[2])
-    for subrep in arbre:
-        for filename in subrep[2]:
+    for filename in os.listdir(sourceDir):
+    #for subrep in arbre:
+    #    for filename in subrep[2]:
             try:
                 with open(sourceDir+filename) as src_jsonfile:
                     with open(destDir+os.path.basename(filename), 'w') as dest_jsonfile:
--- ad-hoc/queryToCSV.py
+++ ad-hoc/queryToCSV.py
@@ -1,21 +1,23 @@
 import pymongo
 import sys
 import csv
-#import json
+import json
 
-#if len(sys.argv) < 4:
-#	sys.exit("USAGE: " + sys.argv[0] + " [sourceCSVfile] [destCSVfile] [columnNumber]")
+if len(sys.argv) < 4:
+	sys.exit("USAGE: " + sys.argv[0] + " 'mongo find dict' 'mongo field dict' [destCSV]")
 
 #Truc chiants:
-# on veut mettre la requete en argument
+# on veut mettre la requete en argument => DONE
 # on veut mettre la collection en argument
 
 c = pymongo.MongoClient()
-find_dict = {"$text":{"$search": "\"Une histoire – art, architecture et design des années 1980 à nos jours\""}}
-field_dict = {"_id":1, "type":1, "title_notice":1, "authors_notice":1, "date_creation":1}
+#find_dict = {"$text":{"$search": "\"Une histoire – art, architecture et design des années 1980 à nos jours\""}}
+#field_dict = {"_id":1, "type":1, "title_notice":1, "authors_notice":1, "date_creation":1}
+find_dict = json.loads(sys.argv[1])
+field_dict = json.loads(sys.argv[2])
 cursor = c.myproject.Artwork.find(find_dict,field_dict)
 
-with open(sys.argv[1], 'w') as f:
+with open(sys.argv[3], 'w') as f:
 	destCSV = csv.writer(f)
 	line_header = []
 	for field in field_dict.keys():
