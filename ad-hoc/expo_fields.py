c399db823d9ca0154e4813c8de68a78177674bea
samaingw <samain.gwenael@laposte.net>
2017-10-30 14:28:33 +0100

    Some regex work

--- ad-hoc/expo_fields.py
+++ ad-hoc/expo_fields.py
@@ -6,56 +6,83 @@ from get_html_list import *
 
 # /!\ This code is **TAB-INDENTED** /!\
 
-
 def extract_date(date_field):
-	regex_date = re.compile('(?:(?:([0-9]{1,2})(?:er)?\\s?)?(?:([\\w]+?)\\s?)?([0-9]{4})?\\s?[\\-–]\\s?)?(?:([0-9]{1,2})(?:er)?\\s?)?(?:([\\w]+?)\\s?)?([0-9]{4})')
+	regex_date = re.compile(r'(?:(?:([0-9]{1,2})(?:er)?\s*)?(?:([\w^\d]+?)\.?\s*)?([0-9]{4})?\s*[\\-–]\s*)?(?:([0-9]{1,2})(?:er)?\s*)?(?:([\w^\d]+?)\.?\s*)?([0-9]{4})')
+	regex_date_fallback = re.compile('.*([0-9]{4})')
 	month2number = ('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre')
+	abbr2number = ('janv', 'fév', 'mars', 'avril', 'mai', 'juin', 'juil', 'août', 'sept', 'oct' 'nov', 'déc')
 	m = regex_date.match(date_field)
 	if m:
 		end_year = m.group(6)
 		end_month = '00'
 		if m.group(5) is not None:
-			end_month = str(month2number.index(m.group(5))+1) if m.group(5) in month2number else '00'
+			month = m.group(5).strip()
+			end_month = str(month2number.index(m.group(5))+1) if m.group(5) in month2number else end_month
+			end_month = str(abbr2number.index(m.group(5))+1) if m.group(5) in abbr2number else end_month
 		end_day = m.group(4) if m.group(4) is not None else '00'
 		start_year = m.group(3) if m.group(3) is not None else end_year
 		start_month = end_month
 		if m.group(2) is not None:
 			start_month = str(month2number.index(m.group(2))+1) if m.group(2) in month2number else end_month
-		start_day = m.group(1) if m.group(1) is not None else end_day
+		if m.group(1) is not None:
+			start_day = m.group(1)
+		elif m.group(2) is not None or m.group(3) is not None:
+			start_day = '00'
+		else:
+			start_day = end_day
+		end_month = end_month if len(end_month) > 1 else '0'+end_month
+		start_month = start_month if len(start_month) > 1 else '0'+start_month
 		#print(start_year+'-'+start_month+'-'+start_day, end_year+'-'+end_month+'-'+end_day)
 		return [(start_year, start_month, start_day), (end_year, end_month, end_day)]
 	else:
-		return None
+		m = regex_date_fallback.match(date_field)
+		if m:
+			return [(m.group(1), '00', '00'), (m.group(1), '00', '00')]
+		else:
+			return None
+
+def get_list_from_html(field):
+	regex_item = re.compile('<li>(.*?)<\/li>', flags=re.S)
+	tab = regex_item.split(field)
+	new_tab = []
+	for item in tab:
+		if len(item) > 0 and 'ul>' not in item and item != '\n':
+			new_tab.append(item)
+	return new_tab
 
 #def extract_expositions(json, field_dict, csvwriter):
 def get_expo_title_other(record):
-	regex_title_other = re.compile('(.*)\\s?:\\s?((?:.*?(?:,| :)\\s?){2}.*[0-9]{4})', flags=re.S)
-	regex_title_other_fallback = re.compile('(.*)\\s?(?::|,)\\s(.* ?[0-9]{4})', flags=re.S)
+	regex_title_other = re.compile(r'(.*)\s*:\s*((?:.*?(?:,| :)\s*){2}.*[0-9]{4})', flags=re.S)
+	regex_title_other_fallback = re.compile(r'(.*)\s*(?::|,)\s+(.* ?[0-9]{4})', flags=re.S)
 	m = regex_title_other.match(record)
 	if m is None:
 		m = regex_title_other_fallback.match(record)
 	if m is None:
 		return None
-	return {'title':m.group(1).strip(), 'other':m.group(2)}
-
+	return {'title':m.group(1).strip() if m.group(1) is not None else None,\
+	'other':m.group(2).strip() if m.group(2) is not None else None}
 
 def get_expo_place_time(placeTimeList):
-	regex_place_time = re.compile('(?:(.*)(?:,\\s|\\s:\\s))?(.*[0-9]{4})?')
-	place_time_list = placeTimeList.split(' // ')
+	regex_place_time = re.compile(r'(?:(.*)(?:,\s+|\s+:\s+))?(.*[0-9]{4})?')
+	place_time_list = placeTimeList.split('//')
 	rslt = []
 	for place_time in place_time_list:
-		m = regex_place_time.match(place_time)
+		m = regex_place_time.match(place_time.strip())
 		if m is None:
 			rslt.append(None)
 		else:
-			rslt.append({'place':m.group(1), 'time':m.group(2)})
+			rslt.append({'place':m.group(1).strip() if m.group(1) is not None else None,\
+			'time':m.group(2).strip() if m.group(2) is not None else None})
 	return rslt
 
 def get_town_museum(place):
-	regex_museum_town = re.compile("(.+?),\\s?([\\w\\-']+\\s?(?:\\(.+?\\))?)$")
-	regex_town_museum = re.compile("([\\w\\-']+\\s?(?:\\(.+?\\))?(?:,\\s[A-Z]{2})?),\\s?(.+?)$")
-	regex_town_museum_fallback = re.compile('([\\w]+,?\\s[\\w]+(?:\\s\\(.+?\\))?),\\s?(.+?)$')
-	regex_museum_town_fallback = re.compile('(.+?),\\s([\\w]+,?\\s[\\w]+(?:\\s\\(.+?\\))?)?$')
+	regex_museum_town = re.compile(r"(.+?),\s*([\w\-']+\s*(?:\(.+?\))?)$")
+	regex_town_museum = re.compile(r"([\w\-']+\s*(?:\(.+?\))?(?:,\s+[A-Z]{2})?),\s*(.+?)$")
+	regex_town_museum_fallback = re.compile(r"([\w\-']+,?\s+[\w\-']+(?:\s+\(.+?\))?),\s*(.+?)$")
+	#regex_museum_town_fallback = re.compile(r"(.+?),\s+([\w\-']+,?\s+[\w\-']+(?:\s+\(.+?\))?)?$")
+	regex_museum_town_fallback = re.compile(r"(.+?),\s+([\w\-']+,?\s+[\w\-']+(?:\s+\(.+?\))?)$")
+	regex_town_museum_fallback2 = re.compile(r"(.+\s*?\(.+?\)),\s*(.+?)$")
+	regex_museum_town_fallback2 = re.compile(r"(.+?),\s*(.+\s*?\(.+?\))$")
 	[town, museum] = ['', '']
 #	rslt = {}
 	o = regex_museum_town.match(place)
@@ -63,10 +90,14 @@ def get_town_museum(place):
 		o = regex_town_museum.match(place)
 		if o is None:
 			o = regex_town_museum_fallback.match(place)
+		if o is None:
+			o = regex_town_museum_fallback2.match(place)
 		if o is not None:
 			[town, museum] = [o.group(1), o.group(2)]
 		else:
 			o = regex_museum_town_fallback.match(place)
+			if o is None:
+				o = regex_museum_town_fallback2.match(place)
 			if o is not None:
 				[town, museum] = [o.group(2), o.group(1)]
 			else:
@@ -74,7 +105,7 @@ def get_town_museum(place):
 					#unplaced_num += 1
 	else:
 		[town, museum] = [o.group(2), o.group(1)]
-	return {'town': town, 'museum': museum}
+	return {'town': town.strip(), 'museum': museum.strip()}
 
 def extract_expositions(json, csvwriter):
 	unmatched_num = 0
